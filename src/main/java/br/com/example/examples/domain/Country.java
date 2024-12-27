231328306ebbbc7eb687f768b8154b217c341938
Jorge Guimarães <jsguimaraes@rd.com.br>
2024-12-27 14:15:54 -0300

    Adiciona configuração H2 database

---
+++ src/main/java/br/com/example/examples/domain/Country.java
@@ -0,0 +1,18 @@
+package br.com.example.examples.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+}
